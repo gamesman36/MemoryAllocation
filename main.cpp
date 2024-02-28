6878d7046f09c67f5812cde95eba86beadf55182
Daniel Andreas Wang <gamesman36@outlook.com>
2024-02-28 21:22:09 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <new>
+
+int main() {
+	int* num_ptr = nullptr;
+	try {
+		num_ptr = new int;
+
+		*num_ptr = 42;
+		std::cout << "Dynamic number: " << *num_ptr << "\n";
+	}
+	catch (const std::bad_alloc& e) {
+		return 1;
+	}
+
+	delete num_ptr;
+
+	return 0;
+}
